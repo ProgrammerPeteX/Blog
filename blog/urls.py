b1f3a10ea79abd8c6b5617acdb83d1002cd00738
Drelich-Tech <Programmer.Pete.X@gmail.com>
2021-01-14 16:14:54 +0100

    testing link back to homepage

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='home'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('odd_page/', views.odd_page, name='odd_page'),
 ]
