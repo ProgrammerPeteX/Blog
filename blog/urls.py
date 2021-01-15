a00e0b8ba7b45135fe0d3146cd92d14315e1aa4e
Drelich-Tech <Programmer.Pete.X@gmail.com>
2021-01-15 19:18:30 +0100

    Finished

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.post_list, name='home'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('odd_page/', views.odd_page, name='odd_page'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
